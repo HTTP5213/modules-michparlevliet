1512b31b921010afab1c076b89aaf8d852bc2498
Michelle Parlevliet <michelle0parlevliet@gmail.com>
2023-03-24 13:27:11 -0400

    create new files

---
+++ public/my-module.js
@@ -0,0 +1,11 @@
+import { 
+  theShire,
+  buckland
+} from "./another-module.js";
+
+let statement = () => `${theShire.name} was founded ${buckland.founding - theShire.founding} years before ${buckland.name}.`;
+
+export {
+  theShire,
+  statement
+}
